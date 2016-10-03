2136e8fa601b7a4b76587277fee48d63aad60d8a
trpeski <antuan.trpeski@hotmail.gr>
2016-10-03 18:47:41 +0300

    Update main.cpp

--- src/loader/main.cpp
+++ src/loader/main.cpp
@@ -1,1 +1,23 @@
 
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+
+
+using namespace std;
+
+int main(int argc,char** argv)
+{
+    fstream file(argv[1]);
+    string line;
+
+    while(getline(file, line))
+    {
+        line = "uname " + line;
+        system(line.c_str());
+    }
+
+
+
+    return 0;
+}
